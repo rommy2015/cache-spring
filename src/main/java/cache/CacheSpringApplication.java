9ec2a4ec22fc47c88d3bbbaef8dd613313f6c238
alex <forn.sky@yandex.ru>
2020-09-26 15:50:46 +0300

    init project

---
+++ src/main/java/cache/CacheSpringApplication.java
@@ -0,0 +1,13 @@
+package cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheSpringApplication.class, args);
+    }
+
+}
